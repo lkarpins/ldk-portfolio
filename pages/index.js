c4f11b741c868e0609fd466f7d967c1fd64af72a
Lidia Karpinski <lidiakarpinski@gmail.com>
2023-03-01 14:15:26 -0500

    Add animated background

--- pages/index.js
+++ pages/index.js
@@ -10,6 +10,14 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <div class="bg-gray-50 min-h-screen flex items-center justify-center px-16">
+        <div class="relative w-full max-w-lg">
+          <div class="absolute top-0 -left-4 w-72 h-72 bg-[#b56576] rounded-full mix-blend-multiply filter blur-xl opacity-70 animate-blob"></div>
+          <div class="absolute top-0 -right-4 w-72 h-72 bg-[#355070] rounded-full mix-blend-multiply filter blur-xl opacity-70 animate-blob animation-delay-2000"></div>
+          <div class="absolute -bottom-8 left-20 w-72 h-72 bg-[#6d597a] rounded-full mix-blend-multiply filter blur-xl opacity-70 animate-blob animation-delay-4000"></div>
+          <div class="m-8 relative space-y-4"></div>
+        </div>
+      </div>
     </>
   );
 }
