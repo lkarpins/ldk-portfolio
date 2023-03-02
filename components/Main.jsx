7f72ecfcacc65cd794037006f21c4c6b96dc391c
Lidia Karpinski <lidiakarpinski@gmail.com>
2023-03-01 19:36:07 -0500

    Add change to positioning and hiding animation to prevent block from buttons

--- components/Main.jsx
+++ components/Main.jsx
@@ -1,27 +1,43 @@
 import React from "react";
+import { AiOutlineMail } from "react-icons/ai";
+import { BsFillPersonLinesFill } from "react-icons/bs";
+import { FaGithub, FaLinkedinIn } from "react-icons/fa";
 
 const Main = () => {
   return (
-    <div className="w-full h-screen ">
-      <div className="  bg-gray-50 min-h-screen flex items-center justify-center px-8">
-        <div className="relative w-full max-w-lg">
+    <div className="w-full h-screen text-center bg-gray-40">
+      <div className=" max-w-[1240px] w-full h-full mx-auto p-2 min-h-screen flex items-center justify-center ">
+        <div className="hidden md:flex relative w-full max-w-lg">
           <div className="absolute top-0 -left-4 w-72 h-72 bg-purple-300 rounded-full mix-blend-multiply filter blur-xl opacity-70 animate-blob"></div>
           <div className="absolute top-0 -right-4 w-72 h-72 bg-yellow-300 rounded-full mix-blend-multiply filter blur-xl opacity-70 animate-blob animation-delay-2000"></div>
           <div className="absolute -bottom-8 left-20 w-72 h-72 bg-pink-300 rounded-full mix-blend-multiply filter blur-xl opacity-70 animate-blob animation-delay-4000"></div>
         </div>
-
-        <div className="flex flex-col justify-start max-width-[500px]  ">
+        <div>
           <h2 className=" tracking-widest text-gray-600">Hi! My name is</h2>
           <h1 className="py-4 text-black">Lidia Karpinski.</h1>
           <h2 className="py-2 text-slate-600">
             I'm a Frontend Software Engineer.
           </h2>
-          <p>
+          <p className="py-4 text-black max-w-[70%] m-auto">
             As a Speech-Language Pathologist turned Software Engineer, I merged
             a passion for communication and accessibility by creating inclusive
             technology through team collaboration, creative problem solving, and
             empathy to maximize user experience.
           </p>
+          <div className="flex items-center justify-between max-w-[330px] m-auto py-4">
+            <div className="rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-105  ease-in duration-300">
+              <FaLinkedinIn />
+            </div>
+            <div className="rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-105  ease-in duration-300">
+              <FaGithub />
+            </div>{" "}
+            <div className="rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-105  ease-in duration-300">
+              <AiOutlineMail />
+            </div>{" "}
+            <div className="rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-105  ease-in duration-300">
+              <BsFillPersonLinesFill />
+            </div>
+          </div>
         </div>
       </div>
     </div>
