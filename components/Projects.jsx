9db80be635d55764fdbb24fd9b758143173de942
Lidia Karpinski <lidiakarpinski@gmail.com>
2023-03-02 17:18:52 -0500

    Add projects component

---
+++ components/Projects.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Projects = () => {
+  return (
+    <div className="w-full">
+      <div className="max-w-[1240px] mx-auto px-2 py-16">
+        <p className="text-xl tracking-widest uppercase text-purple-500">
+          Projects
+        </p>
+        <h2>What I've Built</h2>
+
+        <div className="grid md:grid-cols-2 gap-8"></div>
+      </div>
+    </div>
+  );
+};
+
+export default Projects;
