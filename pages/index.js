07943a3c86879b2ddfeb1233832ed92998c3ccc4
Lidia Karpinski <lidiakarpinski@gmail.com>
2023-03-03 18:15:29 -0500

    Add global styles to document files

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,6 @@
 import About from "@/components/About";
 import Contact from "@/components/Contact";
 import Main from "@/components/Main";
-import Navbar from "@/components/Navbar";
 import Projects from "@/components/Projects";
 import Skills from "@/components/Skills";
 import Head from "next/head";
@@ -19,7 +18,6 @@ export default function Home() {
           rel="stylesheet"
         ></link>
       </Head>
-      <Navbar />
       <Main />
       <About />
       <Skills />
--- pages/roedtoaid.jsx
+++ pages/roedtoaid.jsx
@@ -1,7 +1,43 @@
 import React from "react";
+import Image from "next/image";
+import Roed from "../public/assets/projects/roedtoaid.png";
 
 const roedtoaid = () => {
-  return <div>roedtoaid</div>;
+  return (
+    <div className="w-full">
+      <div className="w-screen h-[30vh] lg:h-[40vh] relative">
+        <div className="absolute top-0 left-0 w-full h-[30vh] lg:h-[40vh] bg-black/80 z-10" />
+        <Image
+          className="absolute z-1"
+          layout="fill"
+          objectFit="cover"
+          src={Roed}
+          alt="/"
+        />
+        <div className="absolute top-[70%] max-w-[1240px] w-full left-[50%] right-[50%] translate-x-[-50%] translate-y-[-50%] text-white z-10 p-2">
+          <h2 className="py-2">Roe'd To Aid</h2>
+          <h3> React JS / React Router / Apollo GraphQL</h3>
+        </div>
+      </div>
+      <div className="max-w-[1240px] mx-auto p-2 grid md:grid-cols-5 gap-8 pt-8">
+        <div className="col-span-4">
+          <p>Project</p>
+          <h2>Overview</h2>
+          <p>
+            Discuss why you made it, who you made it with, what you learned, and
+            obstacles along the way.
+          </p>
+          <button className="px-8 py-2 mt-4 mr-8">Demo</button>
+          <button className="px-8 py-2 mt-4">Code</button>
+        </div>
+        <div className="col-span-4 md:col-span-1 shadow-xl shadow-gray-400 rounded-xl p-4">
+          <div className="p-2">
+            <p>Technologies</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default roedtoaid;
