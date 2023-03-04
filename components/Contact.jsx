b1a4fe081ce8750f0f54d4ee8d985b8185951c38
Lidia Karpinski <lidiakarpinski@gmail.com>
2023-03-04 10:08:39 -0500

    Fixed bug with classname on contact form

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -57,7 +57,7 @@ const Contact = () => {
                   <div className="flex flex-col">
                     <label className="uppercase text-sm py-2">Name</label>
                     <input
-                      classname="border-2 rounded-lg p-3 flex border-gray-300"
+                      className="border-2 rounded-lg p-3 flex border-gray-300"
                       type="text"
                     />
                   </div>
