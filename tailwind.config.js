9d32b0a9169d8d09722708d09c8c982655453a1a
Lidia Karpinski <lidiakarpinski@gmail.com>
2023-03-09 11:33:32 -0500

    Change dimensions of transform so navbar not affected

--- tailwind.config.js
+++ tailwind.config.js
@@ -20,7 +20,7 @@ module.exports = {
             transform: "translate(0px, 0px) scale(1)",
           },
           "33%": {
-            transform: "translate(30px, -50px) scale(1.1)",
+            transform: "translate(20px, -40px) scale(.9)",
           },
           "66%": {
             transform: "translate(-20px, 20px) scale(0.9)",
